f16ad615005178ef833e9b18d9be31b40cba42f0
LESdylan <dev.pro.photo@gmail.com>
2026-03-25 08:40:00 +0100

    refactor(hooks): add barrel export for all 6 custom hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,7 @@
-export { useStudioNavigation } from './useStudioNavigation';
-export { useComponentState } from './useComponentState';
-export { useSearch } from './useSearch';
+export { useClickOutside } from './useClickOutside';
+export { useDebounce } from './useDebounce';
+export { useThrottle } from './useThrottle';
+export { useMediaQuery } from './useMediaQuery';
+export { useBreakpoint } from './useBreakpoint';
+export type { Breakpoint } from './useBreakpoint';
+export { useLocalStorage } from './useLocalStorage';
