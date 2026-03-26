da2ab3d0cc6395f60664169be48ad19e62ea854b
LESdylan <dev.pro.photo@gmail.com>
2026-03-26 18:18:00 +0100

    feat(database_component): add view re-export barrel from lib

---
+++ database_component/src/views/index.ts
@@ -0,0 +1,15 @@
+// Re-export all views from the library (DRY)
+export {
+  TableView,
+  BoardView,
+  ListView,
+  GalleryView,
+  CalendarView,
+  TimelineView,
+  ChartView,
+  FeedView,
+  MapView,
+  DashboardView,
+  displayValue,
+} from '@libcss/components/layout/view';
+export type { DatabaseViewProps } from '@libcss/components/layout/view';
