0aceabd912260de1d18fc111a75e73e60c0e3c20
LESdylan <dev.pro.photo@gmail.com>
2026-03-27 12:05:00 +0100

    feat(cloud-terminal): add CloudTerminal interactive component

---
+++ libcss/components/cloud-terminal/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Cloud Terminal - Public exports
+ */
+
+export { Shell } from './Shell';
+export { ShellModal } from './ShellModal';
+export { ShellFab } from './ShellFab';
+export type { CommandResult, HistoryEntry } from './types';
